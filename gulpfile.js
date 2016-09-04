2f2a276d68668ed564aea1eee33ac91681c242a1
Nikita Vanin <nikitavanin94@gmail.com>
2016-09-04 21:42:10 +0300

    Gulp 4.0, автопрефиксер

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
@@ -19,6 +20,7 @@ gulp.task('styles', function() {
   return gulp.src('source/styles/styles.scss', {base: 'source'})
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
+    .pipe(autoprefixer())
     .pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
     .pipe(gulp.dest('build'));
 
@@ -28,4 +30,10 @@ gulp.task('clean', function() {
   return del(['build/*', '!build/README.md']);
 });
 
-gulp.task('build', ['clean', 'styles']);
+gulp.task('build', gulp.series('clean', 'styles'));
+
+gulp.task('watch', function() {
+  gulp.watch('source/**/*.scss', gulp.series('styles'));
+});
+
+gulp.task('dev', gulp.series('build', 'watch'));
