8536aff94214f67c110d28931f67eeb7b9b213db
Nikita Vanin <nikitavanin94@gmail.com>
2016-09-11 16:06:19 +0300

    Добавил конкатинацию и минификацию для js

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,19 @@ const del = require('del');
 const autoprefixer = require('gulp-autoprefixer');
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
+const fs = require('fs');
 
 // NODE_ENV=production gulp [задача] - запустит задачу в production моде
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+if (isDevelopment) {
+  console.log('**DEVELOPMENT**');
+ } else {
+  console.log('**PRODUCTION**');
+ }
+
+//объект с компонентами
+ let components = getComponentsFiles();
 
 gulp.task('styles', function() {
 
@@ -51,13 +61,12 @@ gulp.task('html', gulp.series('html:index', 'html:templates'));
 
 gulp.task('javascript', function() {
 
-  //собирать компоненты и подключать в нужном порядке
-  return gulp.src(/*файлы*/)
+  return gulp.src(components.js)
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(concat('scripts.min.js'))
     .pipe(gulpIf(!isDevelopment, uglify()))
     .pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
-    .pipe(gulp.dest('build/javascript/'));
+    .pipe(gulp.dest('build/scripts/'));
 
 });
 
@@ -75,13 +84,55 @@ gulp.task('serve', function() {
   browserSync.watch('build/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('build', gulp.series('clean', 'styles', 'html'));
+gulp.task('build', gulp.series('clean', 'styles', 'html', 'javascript'));
 
 gulp.task('watch', function() {
   gulp.watch('source/**/*.scss', gulp.series('styles'));
   gulp.watch('source/**/*.html', gulp.series('html'));
+  gulp.watch('source/**/*.js', gulp.series('javascript'));
 });
 
 gulp.task('develop',
   gulp.series('build', gulp.parallel('watch', 'serve'))
 );
+
+function getComponentsFiles() {
+
+  let componentsFiles = {
+    scss: [],
+    js: [],
+  }
+
+  let connectManager = fs.readFileSync('source/styles/styles.scss', 'utf-8');
+
+  let fileSystem = connectManager.split('\n').filter(function(item) {
+    if(/^(\s*)@import '\.\.\/components/.test(item)) return true;
+    return false;
+  });
+
+  fileSystem.forEach(function(item, i) {
+
+    let componentData = /\/components\/(.+?)\/.+?.scss/.exec(item);
+
+    //название компонента без расширения
+    let componentName = componentData[1];
+    //путь компонента
+    let componentPath = 'source/components/' + componentName;
+    //скрипт компонента
+    let componentJs = componentPath + '/' + componentName + '.js';
+    //если скрпит компонента существует, добавим в сборку
+    if (isFileExist(componentJs)) {
+      componentsFiles.js.push(componentJs)
+    }
+
+  });
+
+  return componentsFiles;
+
+}
+
+//проверка на существование файла
+function isFileExist(path) {
+  if (fs.statSync(path).size > 1) return true;
+  return false;
+}
