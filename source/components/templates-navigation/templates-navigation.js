7625ab2451cd2f6c99751fd0d36de3c330552a2f
Nikita Vanin <nikitavanin94@gmail.com>
2016-09-14 23:17:18 +0300

    Добавил "активное состояние" для ссылок на шаблоны

--- source/components/templates-navigation/templates-navigation.js
+++ source/components/templates-navigation/templates-navigation.js
@@ -2,7 +2,18 @@ ready(function(){
   (function(){
     let templatesNavigation = document.querySelectorAll('.templates-navigation');
     let templatesNavigationToggle = document.querySelectorAll('.templates-navigation__toggle');
+    let templatesNavigationLink = document.querySelectorAll('.templates-navigation__link');
+    let currentUrlPath = window.location.pathname;
 
+    // проверка на текущий шаблон
+    Array.prototype.forEach.call(templatesNavigationLink, function(el, i) {
+      if (el.getAttribute('href') == currentUrlPath) {
+        el.classList.add('templates-navigation__link_current')
+        return;
+      }
+    });
+
+    // проверка состояния панели навигации
     if (
         localStorage.getItem('templates-navigation') == 'templates-navigation_visible'
         || localStorage.getItem('templates-navigation') == null
